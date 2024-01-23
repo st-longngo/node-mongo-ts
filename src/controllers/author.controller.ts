2ddf7270b65169ec0b8281d8e23148d0724be6b8
st-longngo <long.ngo@supremetech.vn>
2024-01-23 16:12:51 +0700

    Handle api of author

--- src/controllers/author.controller.ts
+++ src/controllers/author.controller.ts
@@ -1,75 +1,130 @@
 import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
 
+import { HttpErrors, ApiError } from '../libs';
 import { authorService } from '../services';
 import { IAuthor } from '../interfaces';
-import { authorValidation } from '../validations/author.validation';
 import { customResponse, catchAsync } from '../utils';
 
 export const getAuthors = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-  const authors = await authorService.getAuthors();
+  try {
+    const authors = await authorService.getAllAuthor();
 
-  res.status(httpStatus.OK).send(
-    customResponse<typeof authors>({
-      success: true,
-      error: false,
-      message: authors?.length ? 'Successful Found Authors' : 'No Author found',
-      status: httpStatus.OK,
-      data: authors,
-    })
-  );
+    res.status(httpStatus.OK).send(
+      customResponse<typeof authors>({
+        message: authors?.length ? 'Successful Found Authors' : 'No Author found',
+        status: httpStatus.OK,
+        data: authors,
+      })
+    );
+  } catch (error: any) {
+    throw HttpErrors.INTERNAL_SERVER_ERROR;
+  }
 });
 
 export const getAuthor = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-  const id = req.params.id;
-  const author = await authorService.getAuthorById(id);
+  const authorId = req.params.authorId;
+  try {
+    const author = await authorService.getAuthorById(authorId);
 
-  // Check Author exists
-  if (!author) {
-    res.status(httpStatus.NOT_FOUND).send('Author not found');
+    // Check author exists
+    if (!author) {
+      return next(new ApiError(httpStatus.NOT_FOUND, 'Author not found'));
+    }
+
+    res.status(httpStatus.OK).send(
+      customResponse<typeof author>({
+        message: 'Successful found author',
+        status: httpStatus.OK,
+        data: author,
+      })
+    );
+  } catch (error: any) {
+    throw HttpErrors.INTERNAL_SERVER_ERROR;
   }
-
-  res.status(httpStatus.OK).send(
-    customResponse<typeof author>({
-      success: true,
-      error: false,
-      message: 'Successful Found Author',
-      status: httpStatus.OK,
-      data: author,
-    })
-  );
 });
 
 export const createAuthor = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-  const data = {
-    name: req?.body?.name,
-    birth: req?.body?.birth,
-    death: req?.body?.death,
-  } as IAuthor;
+  const data: IAuthor = req.body;
 
-  // Validation the request
-  const { error, value } = authorValidation.validate(data);
+  try {
+    // Check author exists
+    if (await authorService.getAuthorByName(data.name)) {
+      return next(new ApiError(httpStatus.BAD_REQUEST, 'Author already exists'));
+    }
 
-  if (error) {
-    res.status(httpStatus.BAD_REQUEST).send(error.message);
+    const newAuthor = await authorService.createAuthor(data);
+
+    res.status(httpStatus.CREATED).send(
+      customResponse<typeof newAuthor>({
+        message: 'Successful create author',
+        status: httpStatus.CREATED,
+        data: newAuthor,
+      })
+    );
+  } catch (error: any) {
+    throw HttpErrors.INTERNAL_SERVER_ERROR;
   }
-
-  const newAuthor = await authorService.createAuthor(data);
-  res.status(httpStatus.CREATED).send(
-    customResponse<typeof newAuthor>({
-      success: true,
-      error: false,
-      message: 'Successful Create Author',
-      status: httpStatus.CREATED,
-      data: newAuthor,
-    })
-  );
 });
 
-export const updateAuthor = catchAsync(() => {
-  authorService.updateAuthorById();
+export const updateAuthor = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const authorId = req?.params?.authorId;
+  const data: IAuthor = req.body;
+
+  try {
+    const newAuthor = await authorService.updateAuthorById(authorId, data);
+
+    if (!newAuthor) {
+      return next(new ApiError(httpStatus.NOT_FOUND, 'Author not found'));
+    }
+
+    res.status(httpStatus.OK).send(
+      customResponse<typeof newAuthor>({
+        message: 'Update author successfully',
+        status: httpStatus.OK,
+        data: newAuthor,
+      })
+    );
+  } catch (error: any) {
+    throw HttpErrors.INTERNAL_SERVER_ERROR;
+  }
 });
 
-export const deleteAuthor = catchAsync(() => {
-  authorService.deleteAuthorById();
+export const putAuthor = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const authorId = req?.params?.authorId;
+  const data: IAuthor = req.body;
+
+  try {
+    const newAuthor = await authorService.putAuthorById(authorId, data);
+
+    if (!newAuthor) {
+      return next(new ApiError(httpStatus.NOT_FOUND, 'Author not found'));
+    }
+
+    res.status(httpStatus.OK).send(
+      customResponse<typeof newAuthor>({
+        message: 'Upsert author successfully',
+        status: httpStatus.OK,
+        data: newAuthor,
+      })
+    );
+  } catch (error: any) {
+    throw HttpErrors.INTERNAL_SERVER_ERROR;
+  }
+});
+
+export const deleteAuthor = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const authorId = req?.params?.authorId;
+
+  try {
+    const author = await authorService.deleteAuthorById(authorId);
+
+    if (!author) {
+      return next(new ApiError(httpStatus.NOT_FOUND, 'Author not found'));
+    }
+
+    res.status(httpStatus.NO_CONTENT).send();
+  } catch (error: any) {
+    throw HttpErrors.INTERNAL_SERVER_ERROR;
+  }
 });
